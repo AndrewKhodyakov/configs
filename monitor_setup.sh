fb1a6f710ed3f79de229236b22c74d257ec66711
Khodyakov.And <Khodyakov.And@amonitoring.ru>
2016-10-10 18:26:43 +0300

    Добавил конфигурашку для i3wm

---
+++ monitor_setup.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+#setup screens
+
+#set screens
+xrandr --output HDMI2 --auto --primary --output VGA1 --auto --right-of HDMI2
+#restart i3wm
+
+#set background
+feh --bg-fill ~/Изображения/IMG8444.jpg
