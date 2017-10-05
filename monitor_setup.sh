453badfe9b3c5407dad7b5b912d9004b82957eb1
ajet <andrew.khodyakov@bostongene.com>
2017-10-05 10:44:38 +0300

    Fix bugs and change keys in command

--- monitor_setup.sh
+++ monitor_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#setup screens
+#setup screens, if you wont have access to this script from ~/.config/i3wm/config - put in to ~/bin
 
 #set screens
-xrandr --output HDMI2 --auto --primary --output VGA1 --auto --right-of HDMI2
+#xrandr --output HDMI2 --auto --primary --output VGA1 --auto --right-of HDMI2
 #restart i3wm
+xrandr --output eDP1 --auto --primary --output DP1 --auto --left-of eDP1
 
 #set background
-feh --bg-fill ~/Изображения/IMG8444.jpg
+#feh --bg-fill ~/Изображения/IMG8444.jpg
